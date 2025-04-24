d80d1c392dea29188ac93278cfb020956c993308
nainglinthant111 <oppositedirection111@gmail.com>
2025-04-24 18:26:03 +0630

    add description

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,17 +1,20 @@
 import nainglinthant from "../public/nainglinthant.png";
 export default function Hero() {
     return (
-        <section className="py-16 md:py-16 flex flex-col md:flex-row items-center justify-between lg:px-12">
+        <section className="py-16 flex flex-col md:flex-row items-center justify-between lg:px-10">
             <div className="md:w-1/2 md:border-l-2 md:pl-2 md:border-gray-700">
                 <h1 className="text-3xl md:text-4xl font-bold mb-4">
                     <span className="block mb-2">Hi 👋,</span>
-                    <span className="block mb-2">My name is</span>
                     <span className="text-[#42446e] dark:text-[#d7ffe0]">
-                        Naing Lin Thant
+                        I'm Naing Lin Thant
                     </span>
                 </h1>
-                <p className="text-xl text-gray-600 dark:text-gray-300">
-                    I build things for web
+                <p className="text-md text-justify md:pr-8 text-gray-600 dark:text-gray-300">
+                    I'm a Full Stack Developer passionate about building
+                    efficient and scalable web applications. With expertise in
+                    both front-end and back-end technologies, I strive to create
+                    seamless user experiences and robust server-side
+                    functionalities.
                 </p>
             </div>
             <div className="mt-8 md:mt-0">
