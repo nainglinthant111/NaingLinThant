a971fe3d3c5592028b83a5bd36c5dca05533764c
nainglinthant111 <oppositedirection111@gmail.com>
2025-07-22 23:38:57 +0630

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "@/app/globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
